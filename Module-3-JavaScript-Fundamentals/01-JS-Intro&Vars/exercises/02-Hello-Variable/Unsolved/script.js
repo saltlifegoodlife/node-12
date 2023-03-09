886fd9cf1d002e263cea6a5ee4232b1718def9c1
saltlifegoodlife <saltlifegoodlife@gmail.com>
2023-03-09 14:56:51 -0600

    exercises

--- Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/02-Hello-Variable/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/02-Hello-Variable/Unsolved/script.js
@@ -1,1 +1,15 @@
 // Write Your JavaScript Code Here
+let personName = "Ross Beckham";
+let pets = 2;
+let funFact = "I go kayak fishing out in the ocean!";
+
+console.log("My name is " + personName + "!");
+console.log(`I have ${pets} pet(s).`);
+console.log(`FUN FACT: ${funFact}`);
+
+personName = "Matt P";
+funFact = "I like Basketball!";
+
+console.log(`My name is ${personName}!`);
+console.log(`I have ${pets} pet(s).`);
+console.log(`FUN FACT: ${funFact}`);
--- Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/04-Scope/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/04-Scope/Unsolved/script.js
@@ -1,11 +1,12 @@
 // TODO: Declare variable 'shout' with the value 'Shout' so it's available to `justShout` and `shoutItAllOut` functions
+const shout = "Shout";
 function justShout() {
   console.log(shout + ", " + shout);
   return;
 }
 
 function shoutItAllOut() {
-  console.log(shout + " it all out! ");
+  console.log(shout + " const it all out! ");
   return;
 }
 
@@ -13,24 +14,25 @@ justShout();
 shoutItAllOut();
 
 // TODO: Declare variable 'animal' with the value 'Tigers' so it is only available to the 'sayTigers' function
+
 function sayLions() {
-  var animal = "Lions"; 
+  let animal = "Lions";
   console.log(animal);
   return;
 }
 
+let animal = "Tigers";
 function sayTigers() {
-  console.log("and " + animal + " and "); 
+  console.log("and " + animal + " and ");
   return;
 }
 
-// TODO: The variable 'bears' should only declared once and 'sayBears' should return "Bears! OH MY!". 
-var bears = "Bears";
+// TODO: The variable 'bears' should only declared once and 'sayBears' should return "Bears! OH MY!".
+const bears = "Bears";
 
 function sayBears() {
-    var bears = "Pandas";
-    console.log(bears + "! OH MY!");
-    return;
+  console.log(bears + "! OH MY!");
+  return;
 }
 
 sayLions();
@@ -38,13 +40,16 @@ sayTigers();
 sayBears();
 
 // TODO: The variable 'sing' should be declared once in the local scope.
-var sing = "Sing"; 
 
 function singAlong() {
+  const sing = "Sing";
+
   console.log(sing + ",");
-  var singASong = function () {
+
+  const singASong = function () {
     console.log(sing + " a Song.");
   };
+
   singASong();
 }
 
