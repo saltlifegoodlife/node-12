e8aeada83443e1cfe5be9a7eaccc209e321f50e1
saltlifegoodlife <saltlifegoodlife@gmail.com>
2023-03-14 13:25:50 -0500

    worked on exercises

--- Module-3-JavaScript-Fundamentals/03-JS-Selection-Statements/exercises/01-Conditional-Statements/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/03-JS-Selection-Statements/exercises/01-Conditional-Statements/Unsolved/script.js
@@ -1,6 +1,21 @@
 // Change the values and operators below to test your algorithm meets all conditions
 var x = 50;
-var expression1 = (x < 25);
-var expression2 = (x > 50);
+var expression1 = x < 25;
+var expression2 = x > 50;
 
 // Write Your JavaScript Code Here
+if (!expression1 && !expression2) {
+  console.log("False ❌ False ❌");
+}
+
+if (!expression1 || expression2) {
+  console.log("False ❌ True ✅");
+}
+
+if (expression1 || !expression2) {
+  console.log("True ✅ True  ❌");
+}
+
+if (expression1 && expression2) {
+  console.log("True ✅ True ✅");
+}
