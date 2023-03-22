0d6ca8dab149d165abd02a3e89baab097222c14a
saltlifegoodlife <saltlifegoodlife@gmail.com>
2023-03-22 09:02:00 -0500

    practice

--- Module-3-JavaScript-Fundamentals/05-JS-Functions/exercises/01-Functions/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/05-JS-Functions/exercises/01-Functions/Unsolved/script.js
@@ -1,11 +1,11 @@
 // isEqual using function declaration
 function isEqual(x, y) {
   if (x === y) {
-    console.log('They are equal in type and value');
+    console.log("They are equal in type and value");
   } else if (x == y) {
-    console.log('They are equal in value');
+    console.log("They are equal in value");
   } else {
-    console.log('They are not equal');
+    console.log("They are not equal");
   }
   return;
 }
@@ -16,9 +16,9 @@ isEqual(10, 10);
 // Refer to sample functions as needed!
 
 // TODO: Call the isEqual function so that it logs "They are equal in value"
-
+isEqual(5, 5);
 // TODO: Rewrite isEqual as a function expression called 'isEqualTakeTwo`
+let isEqualTakeTwo = isEqual;
 
 // TODO: Call the isEqualTakeTwo function so that it logs "They are not equal"
-
-
+isEqualTakeTwo(5, "Hi");
