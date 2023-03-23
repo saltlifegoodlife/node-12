bed084f908baf3974dfb2c7c90d3ad4d679f8b73
saltlifegoodlife <saltlifegoodlife@gmail.com>
2023-03-23 09:07:31 -0500

    something

--- Module-04-Advanced-JS/02-JS-OOP/exercises/01-Constructors/Unsolved/index.js
+++ Module-04-Advanced-JS/02-JS-OOP/exercises/01-Constructors/Unsolved/index.js
@@ -1,7 +1,15 @@
 // TODO: Create a constructor function called 'Developer' that takes in 'name' and 'tech'
+function Developer(name, tech) {
+  this.name = name;
+  this.tech = tech;
 
+  this.introduction = () => {
+    console.log(`This is ${this.name} and his favorite tech is ${this.tech}!`);
+  };
+}
 // TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
 
 // TODO: Create a new object using the 'Developer' constructor
-
+const mattJ = new Developer("Matt J", "React");
 // TODO: Call the 'introduction()' method on the new object
+mattJ.introduction();
--- Module-04-Advanced-JS/04-JS-DOM/exercises/01-Traverse-DOM/Unsolved/assets/js/script.js
+++ Module-04-Advanced-JS/04-JS-DOM/exercises/01-Traverse-DOM/Unsolved/assets/js/script.js
@@ -1,7 +1,8 @@
 // Access element using id
-var articlesDiv = document.getElementById('articles');
-var headerDiv = document.getElementById('header');
-
+var articlesDiv = document.getElementById("articles");
+var headerDiv = document.getElementById("header");
+const article1 = document.getElementById("article1");
+const welcome = document.getElementById("welcome");
 // Change style by accessing style object's properties
-articlesDiv.children[2].style.fontSize = '50px';
-headerDiv.style.color = 'white';
+article1.style.fontSize = "50px";
+headerDiv.children[0].style.color = "white";
