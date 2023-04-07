b31fb4e38b0adfaac2d21a4d2d372798c2697ce2
saltlifegoodlife <saltlifegoodlife@gmail.com>
2023-04-07 09:25:39 -0500

    pushing

--- zeAlgos/03-is-palindrome/Unsolved/is-palindrome.js
+++ zeAlgos/03-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,6 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function (str) {
+  let check = str.split("").reverse().join("");
+  return str == check;
+};
