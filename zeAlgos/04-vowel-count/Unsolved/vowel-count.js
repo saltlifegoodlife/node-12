e0bd0725d5e2a5293dbba09a75d78b900fe2405a
saltlifegoodlife <saltlifegoodlife@gmail.com>
2023-04-10 09:04:43 -0500

    push

--- zeAlgos/04-vowel-count/Unsolved/vowel-count.js
+++ zeAlgos/04-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,17 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function (str) {
+  //   let count = 0;
+  //   str
+  //     .toLowerCase()
+  //     .split("")
+  //     .map((e) => {
+  //       if (e == "a" || e == "e" || e == "i" || e == "o" || e == "u") {
+  //         count++;
+  //       }
+  //     });
+  //   return count;
+
+  let reg = /([aeiou])+/gi;
+  return str == "" ? 0 : str.match(reg).length;
+};
